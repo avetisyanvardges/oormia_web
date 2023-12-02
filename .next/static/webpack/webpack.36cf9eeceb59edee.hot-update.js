49025dd03d2c7c8479287aefcd74d11f99995477
RafoPetrosyan <petrosyanrafo0@gmail.com>
2023-12-02 14:58:39 +0400

    responsive styles

--- .next/static/webpack/webpack.36cf9eeceb59edee.hot-update.js
+++ .next/static/webpack/webpack.36cf9eeceb59edee.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d6deb35d23cbbda3"; }
+/******/ 	__webpack_require__.h = function() { return "1769296f4f49e763"; }
 /******/ }();
 /******/ 
 /******/ }
--- views/Home/index.js
+++ views/Home/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {useWindowSize} from "../../hook/useWindowSize";
+
 const Home = () => {
-    const size = useWindowSize()
     return (
-        <div className='home-page' style={{width: size.width, height: size.height}}>
-            <div style={{display: 'flex', flexDirection: 'row',paddingTop: 50,alignItems: 'center'}}>
-                <div style={{width: '100%',height:'100%',paddingLeft: 24}}>
-                    <h1 className='title'>OOrmia - Explore | Learn | Connect</h1>
-                    <p className='description'>
+        <div className='home-page'>
+            <div className='content'>
+                <div className='desc-content'>
+                    <h1 className='title montserrat'>OOrmia - Explore | Learn | Connect</h1>
+                    <p className='description montserrat'>
                         <b>Experience your community in a whole new way with OOrmia! Our app is your key to discovering
                         local events and connecting with like-minded individuals. Receive personalized event suggestions
                         based on your preferences, ensuring every day is filled with exciting opportunities. Join
@@ -20,20 +19,18 @@ const Home = () => {
                         <b>- Bring your friends together! Make your own groups, and we'll find cool events that everyone will like. It's all about what you enjoy.</b><br/><br/>
                     </p>
                 </div>
-                <div style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginRight: 24}}>
-                    <img src="/assets/svgs/welcome.svg"
-                         style={{width: '40%',  left: 80, position: 'relative'}}/>
-                    <img src="/assets/svgs/map.svg" style={{width: '38%',  marginTop: '2%'}}/>
+                <div className='image-content'>
+                    <img src="/assets/svgs/welcome.svg" className='welcome animate__animated animate__bounceInDown' alt=''/>
+                    <img src="/assets/svgs/map.svg" className='map animate__animated animate__fadeInRight' alt=''/>
                 </div>
             </div>
-            <div>
-                <div style={{display: 'flex', justifyContent: 'flex-start', alignItems: 'center', marginLeft: 24,marginTop: 50}}>
-                    <img src="/assets/svgs/GooglePlay.svg"
-                         style={{width: 150, height: 60,marginRight: 24}}/>
-                    <img src="/assets/svgs/AppStore.svg" style={{width: 150, height: 60}}/>
+            <div className='bottom-content'>
+                <div className='stores'>
+                    <img src="/assets/svgs/GooglePlay.svg" className='google-play' alt=''/>
+                    <img src="/assets/svgs/AppStore.svg" className='app-store' alt=''/>
                 </div>
-                <div style={{flex: 1,marginTop: 50,marginLeft: 24}}>
-                    <p style={{color: '#fff'}}>Copyright ©2023 OOrmia. All rights are reserved</p>
+                <div className='reserved-content'>
+                    <p className='text'>Copyright ©{new Date().getFullYear()} OOrmia. All rights are reserved</p>
                 </div>
             </div>
         </div>
